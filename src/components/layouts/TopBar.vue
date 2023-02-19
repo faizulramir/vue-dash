9d7638cac274169ca051c21ee0f615abe90394bd
faizulramir <faizul.ramir@gmail.com>
2023-02-20 06:09:48 +0800

    update sidebar, header

---
+++ src/components/layouts/TopBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding-top: 4%;">
+    <div class="container">
+      
+    </div>
+  </nav>
+</template>
+  
+<script>
+  
+  export default {
+    name: 'TopBar',
+    components: {
+      
+    },
+    data() {
+      return {
+       
+      }
+    },
+    methods: {
+      
+    }
+  }
+  </script>
+  
+  <style>
+  </style>
+  
